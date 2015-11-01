359d31be761c00ce34102fa0e6d5c6be15dc6793
Michel Felipe <mfelipeof@gmail.com>
2015-11-01 01:21:46 -0300

    Base platform game structure simulating infinity running

---
+++ Assets/Scripts/MoveScene.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveScene : MonoBehaviour {
+
+	public float speed;
+	private float x;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		x = transform.position.x;
+		x += speed * Time.deltaTime;
+
+		transform.position = new Vector3 (x, transform.position.y, transform.position.z);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public Rigidbody2D playerRigid;
+	public Animator playerAnimator;
+	public int forceJump;
+
+	//Verify if character touch the ground
+	public bool grounded;
+	public Transform groundCheck;
+	public LayerMask whatIsGround;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButtonDown("Jump") && grounded) {
+			playerRigid.AddForce(new Vector2(50,forceJump));
+		}
+
+		grounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, whatIsGround);
+
+		playerAnimator.SetBool("jump", !grounded);
+	}
+}
