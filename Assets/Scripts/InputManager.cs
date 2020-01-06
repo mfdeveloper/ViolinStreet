b35afe8814c5a48243101bdeef10fdaa04b21f32
Felipe <mfelipeof@gmail.com>
2020-01-06 05:14:46 +0000

    Added attack and update jump to each screen sides

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//TODO: Merge this class with InputManager class created Death Hawai Ludum Dare Jam game
+/// <summary>
+/// Class to manage extra generic cross inputs (mobile/mouse) mapping, for example
+/// </summary>
+public static class InputManager
+{
+    public class TouchOrMouse
+    {
+        public bool IsScreendLeft()
+        {
+            Vector2 position = GetPosition();
+            if (position != Vector2.zero && position.x < Screen.width / 2)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool IsScreenRight()
+        {
+            Vector2 position = GetPosition();
+
+            if (position != Vector2.zero && position.x > Screen.width / 2)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public Vector2 GetPosition()
+        {
+            if (IsTouched)
+            {
+                return unityTouch.position;
+            }
+            else if (Input.GetMouseButtonDown(0))
+            {
+                return Input.mousePosition;
+            }
+
+            return Vector2.zero;
+        }
+    }
+
+    public static TouchOrMouse touch = new TouchOrMouse();
+    public static bool IsTouched {
+        get
+        {
+            if (Input.touchCount > 0)
+            {
+                unityTouch = Input.GetTouch(0);
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    private static Touch unityTouch;
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour
@@ -6,6 +8,12 @@ public class Player : MonoBehaviour
 
     [SerializeField]
     protected float jumpForce = 3f;
+    [SerializeField]
+    protected GameObject attackShoot;
+    [SerializeField]
+    protected float attackSpeed = 2f;
+    [SerializeField]
+    protected bool attackOnAir = true;
 
     protected Rigidbody2D rigidBody;
     protected SpriteRenderer spriteRenderer;
@@ -13,11 +21,20 @@ public class Player : MonoBehaviour
     protected bool jumping = false;
     protected bool isGrounded = false;
     protected bool inAir = false;
+    protected List<GameObject> shoots = new List<GameObject>();
 
     void Awake()
     {
         rigidBody = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if(attackShoot != null)
+        {
+            if(attackShoot.GetComponent<SpriteRenderer>() == null)
+            {
+                Debug.LogError("A Sprite component is required for attack Prefab");
+            }
+        }
     }
 
     // Update is called once per frame
@@ -29,6 +46,8 @@ public class Player : MonoBehaviour
     void FixedUpdate()
     {
         Jump();
+
+        Attack();
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -50,13 +69,30 @@ public class Player : MonoBehaviour
     }
     public virtual void Inputs()
     {
+
         if (!EventSystem.current.IsPointerOverGameObject())
         {
-            if (Input.GetMouseButtonDown(0) && !inAir)
+            if (InputManager.touch.IsScreenRight() && !inAir)
             {
                 jumping = true;
             }
+
+            if (InputManager.touch.IsScreendLeft() && attackShoot != null)
+            {
+
+                if (!attackOnAir)
+                {
+                    if (!inAir)
+                    {
+                        shoots.Add(Instantiate(attackShoot, transform));
+                    }
+                } else
+                {
+                    shoots.Add(Instantiate(attackShoot, transform));
+                }
+            }
         }
+
     }
 
     public virtual void Jump()
@@ -66,6 +102,49 @@ public class Player : MonoBehaviour
             //rigidBody.velocity = Vector2.zero;
             rigidBody?.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             jumping = false;
+        }
+    }
+
+    public virtual void Attack()
+    {
+        if (shoots.Count > 0)
+        {
+            for (int i = 0; i < shoots.Count; i++)
+            {
+                var shoot = shoots[i];
+                if (shoot != null)
+                {
+                    shoot.transform.Translate(Vector2.right * attackSpeed * Time.deltaTime);
+
+                    var shootPos = Camera.main.WorldToViewportPoint(shoot.transform.position);
+                    if (shootPos.x >= 1.0f)
+                    {
+                        Debug.Log("Destroy shoot");
+
+                        shoots.Remove(shoot);
+                        Destroy(shoot);
+                    }
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Callback subscribed by event "OnBeat" of <see cref="MetronomePro.events"/> attribute
+    /// <b>PS:</b> This is ony for player sync tests. Remove this in future
+    /// </summary>
+    /// <param name="metronome"></param>
+    /// <param name="player"></param>
+    public virtual void ChangeColor(MetronomePro metronome, MetronomePro_Player player)
+    {
+        
+        if (transform.childCount > 0)
+        {
+            var child = transform.GetChild(0);
+            child.GetComponentInChildren<SpriteRenderer>().color = Random.ColorHSV(0f, 1f);
+        } else if (spriteRenderer != null)
+        {
+            spriteRenderer.color = Random.ColorHSV(0f, 1f);
         }
     }
 }
