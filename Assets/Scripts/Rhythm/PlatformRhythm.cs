ede1e50f5e11473876086ce90e5c326852faf77a
Felipe <mfelipeof@gmail.com>
2020-01-07 10:06:03 +0000

    [Release] Deliver to the teacher with platform synced with beat music
    [Scene] Added the scenario with backgrounds and platform grounds to jump

---
+++ Assets/Scripts/Rhythm/PlatformRhythm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class PlatformRhythm : MonoBehaviour
+{
+    [NonSerialized]
+    public Animator animatorController;
+
+    void Awake()
+    {
+        animatorController = GetComponent<Animator>();
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        Player player = collision.GetComponent<Player>();
+        if (player != null)
+        {
+            player.PlatformJump = this;
+        }
+    }
+}
